92db1ee9fabcb8a6123857b97a71645d871d034a
Bennett Lin <bennettslin@gmail.com>
2014-08-24 13:43:19 -0700

    Initial Commit

---
+++ GhostWord/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GhostWord
+//
+//  Created by Bennett Lin on 8/24/14.
+//  Copyright (c) 2014 Bennett Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GhostWord/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GhostWord
+//
+//  Created by Bennett Lin on 8/24/14.
+//  Copyright (c) 2014 Bennett Lin. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+            
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
